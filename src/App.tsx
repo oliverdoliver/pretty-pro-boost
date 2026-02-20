cd5b1fcb00d7a5777d39cab9866da2cba7af292c
oliverdoliver <128324472+oliverdoliver@users.noreply.github.com>
2026-02-20 04:54:47 -0800

    Remove StairwellCleaning import and route from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import AcceptInvite from "./pages/AcceptInvite";
-import StairwellCleaning from "./pages/StairwellCleaning";
+
 
 // Portal pages
 import Dashboard from "./pages/portal/Dashboard";
@@ -36,8 +36,8 @@ const App = () => (
             <Route path="/login" element={<Login />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/invite" element={<AcceptInvite />} />
-            <Route path="/trapphusstadning" element={<StairwellCleaning />} />
+            
+            
 
             {/* Portal routes (protected) */}
             <Route path="/portal" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
